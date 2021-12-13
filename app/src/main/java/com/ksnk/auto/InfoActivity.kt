6b61be660156efe4fd295f9bb937235a0dcef3f7
Dmitry Kosenko <71468576+Ksnk-dm@users.noreply.github.com>
2021-12-13 15:25:00 +0200

    fix empty search, fix error search

--- app/src/main/java/com/ksnk/auto/InfoActivity.kt
+++ app/src/main/java/com/ksnk/auto/InfoActivity.kt
@@ -1,7 +1,6 @@
 package com.ksnk.auto
 
 import android.annotation.SuppressLint
-import android.opengl.Visibility
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
@@ -12,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.google.gson.GsonBuilder
 import com.ksnk.auto.entity.Auto
+import com.ksnk.auto.entity.Error
 import com.squareup.picasso.Picasso
 import okhttp3.*
 import java.io.IOException
@@ -34,7 +34,7 @@ class InfoActivity : AppCompatActivity() {
     private var colorTextView: TextView? = null
     private var regCompanyTextView: TextView? = null
     private var infoLinearLayout: LinearLayout? = null
-    private var searhLinearLayout: LinearLayout? = null
+    private var searchLinearLayout: LinearLayout? = null
     private var regOrg: String? = null
     private var searchEditText: EditText? = null
     private var buttonSearch: Button? = null
@@ -57,25 +57,26 @@ class InfoActivity : AppCompatActivity() {
         regCompanyTextView = findViewById(R.id.regCompanyTextView)
         vinCode = findViewById(R.id.vinTextView)
         infoLinearLayout = findViewById(R.id.info_linear_layout)
-        searhLinearLayout = findViewById(R.id.search_linear_layout)
+        searchLinearLayout = findViewById(R.id.search_linear_layout)
         searchEditText = findViewById(R.id.searchNumberEditText)
         buttonSearch = findViewById(R.id.buttonSearch)
         buttonSearch?.setOnClickListener {
-            searchGet(searchEditText?.text.toString())
+            if (searchEditText?.text.isNullOrEmpty()) {
+                Toast.makeText(applicationContext, "Введите номер", Toast.LENGTH_LONG).show()
+            } else {
+                searchGet(searchEditText?.text.toString())
+            }
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
-        // fetchJson()
         init()
         setSupportActionBar(toolbar);
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true);
-
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         imageViewAuto?.setImageResource(R.drawable.searchinfo)
-        supportActionBar?.title = "Поиск по гос номеру"
+        supportActionBar?.title = "Поиск по гос. номеру"
         //  setParams()
 
     }
@@ -89,7 +90,7 @@ class InfoActivity : AppCompatActivity() {
     private fun setParamsSearch(auto: Auto) {
         val autoObj: Auto = auto//intent.getSerializableExtra("auto") as Auto
         supportActionBar?.title = autoObj.getDigits()
-        searhLinearLayout?.visibility = VISIBLE
+        searchLinearLayout?.visibility = VISIBLE
         infoLinearLayout?.visibility = GONE
         if (autoObj.getPhotoUrl().isNullOrEmpty()) {
             imageViewAuto?.setImageResource(R.drawable.ic_placeholder)
@@ -101,7 +102,6 @@ class InfoActivity : AppCompatActivity() {
                 .centerCrop()
                 .into(imageViewAuto)
         }
-
 
         digitsTextView?.text = autoObj.getDigits()
         vinCode?.text = autoObj.getVin()
@@ -121,29 +121,6 @@ class InfoActivity : AppCompatActivity() {
             "Нет"
         }
         regCompanyTextView?.text = regOrg
-        // val auto: Auto = intent.getSerializableExtra("auto") as Auto
-//        supportActionBar?.title = auto.getDigits();
-
-//        Picasso.get()
-//            .load(auto.getPhotoUrl())
-//            .error(R.drawable.bg_gradient)
-//            .fit()
-//            .centerCrop()
-//            .into(imageViewAuto);
-//
-//        digitsTextView?.text = auto.getDigits()
-//        vinCode?.text = auto.getVin()
-//        modelHomeTextView?.text = auto.getVendor() + " " + auto.getModel()
-//        regionTextView?.text = auto.getRegion()?.getName()
-//        vendorTextView?.text = auto.getVendor()
-//        modelTextView?.text = auto.getModel()
-//        yearTextViewTextView?.text = auto.getModelYear().toString()
-//        classAutoTextView?.text = auto.getOperations()?.get(0)?.getKind()?.getRu()
-//        dateRegisterTextView?.text = auto.getOperations()?.get(0)?.getRegisteredAt()
-//        informRegisterTextView?.text = auto.getOperations()?.get(0)?.getOperation()?.getRu()
-//        orgTextView?.text = auto.getOperations()?.get(0)?.getDepartment()
-//        colorTextView?.text = auto.getOperations()?.get(0)?.getColor()?.getRu()
-//        regCompanyTextView?.text = regOrg
     }
 
     private fun searchGet(number: String) {
@@ -160,16 +137,15 @@ class InfoActivity : AppCompatActivity() {
             override fun onResponse(call: Call, response: Response) {
                 mainHandler.post {
                     val body = response.body?.string()
-                    println(body)
                     val gson = GsonBuilder().create()
-                    var auto: Auto? = gson.fromJson(body, Auto::class.java)
-                    setParamsSearch(auto!!)
-                    Log.d("search text get", auto.toString())
-//                    var weather: Air? = gson.fromJson(body, Air::class.java)
-//                    list?.add(weather!!)
-//                    recyclerView?.adapter = AirRecyclerViewAdapter(list!!)
-//                    textviewAqiMain?.setText(weather?.getData()?.get(0)?.getAqi().toString())
-//                    println(weather)
+                    println("test"+body)
+                   var error: Error = gson.fromJson(body, Error::class.java)
+                    if(error.getError().isNullOrEmpty()){
+                        var auto: Auto? = gson.fromJson(body, Auto::class.java)
+                        setParamsSearch(auto!!)
+                    } else {
+                        Toast.makeText(applicationContext, "Авто не найдено", Toast.LENGTH_LONG).show()
+                    }
                 }
             }
 
